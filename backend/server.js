4ec6ab59efae2185620c78abfbe5e3d6d613d2b8
Rajender216 <216rajender@gmail.com>
2025-05-20 17:51:04 +0530

    vercel.json added

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,9 @@ cron.schedule("0 */24 * * *", () => {
   fetchEventsAndSaveToDB();
 });
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
